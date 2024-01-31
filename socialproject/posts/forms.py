6d7800de1d52d9a1038f061662cbd93f2ffbe1d2
ycbq999 <ycbq999@hotmail.com>
2024-01-30 23:49:50 -0800

    s

--- socialproject/posts/forms.py
+++ socialproject/posts/forms.py
@@ -1,8 +1,15 @@
-from .models import Post
+from .models import Post, Comment
 from django import forms
 
 class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post        
         fields = ['title', 'caption', 'image']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body', 'posted_by']  
+
+
         
